4047901b6ac2581f66c4b7192943f0d7b22684cb
Назар Колісник <88043847+NazarKolisnyk@users.noreply.github.com>
2025-05-17 15:58:59 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-fetch('/template/head.html')
+fetch('/agrotern/template/head.html')
     .then(res => res.text())
     .then(data => document.querySelector('head').innerHTML = data);
 
-fetch('/template/header.html')
+fetch('/agrotern/template/header.html')
     .then(res => res.text())
     .then(data => document.getElementById('header').innerHTML = data);
 
-fetch('/template/footer.html')
+fetch('/agrotern/template/footer.html')
     .then(res => res.text())
     .then(data => document.getElementById('footer').innerHTML = data);
